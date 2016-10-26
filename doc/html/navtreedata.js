20d360c9f6ac01eeb797e7ce3132e7e9bb0123c0
Rafael Nájera <rafael.najera@uni-koeln.de>
2016-10-26 17:53:42 +0200

    Merged into master, version 0.04 ready for production

--- doc/html/navtreedata.js
+++ doc/html/navtreedata.js
@@ -1,31 +1,11 @@
 var NAVTREE =
 [
-  [ "APM", "index.html", [
-    [ "Todo List", "todo.html", null ],
-    [ "Data Structures", "annotated.html", [
-      [ "Data Structures", "annotated.html", "annotated_dup" ],
-      [ "Data Structure Index", "classes.html", null ],
-      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Data Fields", "functions.html", [
-        [ "All", "functions.html", null ],
-        [ "Functions", "functions_func.html", null ],
-        [ "Variables", "functions_vars.html", null ]
-      ] ]
-    ] ],
-    [ "Files", null, [
-      [ "File List", "files.html", "files" ],
-      [ "Globals", "globals.html", [
-        [ "All", "globals.html", null ],
-        [ "Functions", "globals_func.html", null ],
-        [ "Variables", "globals_vars.html", null ]
-      ] ]
-    ] ]
-  ] ]
+  [ "APM", "index.html", ]
 ];
 
 var NAVTREEINDEX =
 [
-"annotated.html"
+"index.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';

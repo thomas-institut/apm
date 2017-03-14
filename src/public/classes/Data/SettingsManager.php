49f913c3600f1e1fa4e26d61348377b4659240ed
Rafael Nájera <rafael.najera@uni-koeln.de>
2017-03-14 12:08:07 +0100

    Added SettingsManager

---
+++ src/public/classes/Data/SettingsManager.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ *  Copyright (C) 2017 Universität zu Köln
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *   
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  
+ */
+
+namespace AverroesProject\Data;
+
+use DataTable\InMemoryDataTable;
+/**
+ * Description of SettingsManager
+ *
+ * @author Rafael Nájera <rafael.najera@uni-koeln.de>
+ */
+class SettingsManager {
+    
+    /**
+     *
+     * @var DataTable 
+     */
+    private $settingsTable;
+    
+    public function __construct($table = false) {
+        $this->settingsTable = ($table === false) 
+                ? new InMemoryDataTable() 
+                : $table;
+    }
+    
+    public function getSetting(string $setting)
+    {
+        $id = $this->settingsTable->findRow(['setting' => $setting]);
+        if ($id === false) {
+            return false;
+        }
+        $row = $this->settingsTable->getRow($id);
+        return $row['value'];
+    }
+    
+    public function setSetting(string $setting, string $value)
+    {
+        $id = $this->settingsTable->findRow(['setting' => $setting]);
+        if ($id == false) {
+            return false !== $this->settingsTable->createRow([
+                'setting' => $setting, 
+                'value' => $value]);
+        }
+        return $this->settingsTable->updateRow([ 
+            'id' => $id,
+            'setting' => $setting, 
+            'value' => $value]);
+    }
+}
---
+++ src/public/test/SettingsManagerTest.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ *  Copyright (C) 2017 Universität zu Köln
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *   
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  
+ */
+
+namespace AverroesProject;
+
+require "../vendor/autoload.php";
+
+use PHPUnit\Framework\TestCase;
+use AverroesProject\Data\SettingsManager;
+
+/**
+ * Description of SettingsManagerTest
+ *
+ * @author Rafael Nájera <rafael.najera@uni-koeln.de>
+ */
+class SettingsManagerTest extends TestCase {
+    
+    public function testBasic() 
+    {
+        $sm = new SettingsManager();
+        
+        $r = $sm->getSetting('test');
+        $this->assertFalse($r);
+        
+        $r = $sm->setSetting('setting1', 'somevalue');
+        $this->assertTrue($r);
+        $r = $sm->getSetting('setting1');
+        $this->assertEquals('somevalue', $r);
+        $r = $sm->setSetting('setting1', 'othervalue');
+        $this->assertTrue($r);
+        $r = $sm->getSetting('setting1');
+        $this->assertEquals('othervalue', $r);
+    }
+}

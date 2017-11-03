9d78dc54e2fac8abdde9008ecc43a60a94cc595f
Rafael Nájera <rafael.najera@uni-koeln.de>
2017-11-03 16:22:12 +0100

    DatabaseChecker updated to DB version 12

--- src/public/test/DatabaseCheckerTest.php
+++ src/public/test/DatabaseCheckerTest.php
@@ -63,7 +63,7 @@ EOD;
         
         $tableSetupSQL = <<<EOD
         INSERT INTO `dbchecktest_settings` (`id`, `setting`, `value`) VALUES
-        (1, 'dbversion', '11');
+        (1, 'dbversion', '12');
 EOD;
         self::$dbConn->query($tableSetupSQL);
         

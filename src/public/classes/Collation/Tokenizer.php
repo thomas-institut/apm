6ab55bb8f137bb9b9b0aaa899129203597e27394
Rafael Nájera <rafael.najera@uni-koeln.de>
2018-02-07 12:16:02 +0100

    Implemented text splitter in Tokenizer

--- src/public/classes/Collation/Tokenizer.php
+++ src/public/classes/Collation/Tokenizer.php
@@ -27,11 +27,122 @@ namespace AverroesProject\Collation;
  */
 class Tokenizer {
     
+    const TOKEN_UNDEFINED = 0;
+    const TOKEN_WORD = 1;
+    const TOKEN_WS = 2;
+    const TOKEN_PUNCT = 3;
+    
     private static function isWhiteSpace(string $s) {
         if (trim($s) === '') {
             return true;
         }
         return false;
+    }
+    
+    /**
+     * Splits the given string into an array 
+     * of strings of the following kinds:
+     *  - whitespace
+     *  - punctuation
+     *  - words
+     * @param string $text
+     */
+    public static function splitText(string $text) 
+    {
+        $tokens = [];
+        $currentTokenCharacters = [];
+        $currentTokenType = self::TOKEN_UNDEFINED;
+        $state = 0;
+        $wsRegExp = '/\s+/';
+        $puntRegExp = '/[\.,;:\(\)\[\]¶⊙!]+/';
+        
+        for ($i=0; $i<strlen($text); $i++) {
+            switch($state) {
+                case 0:
+                    if (preg_match($wsRegExp, $text[$i])) {
+                        $currentTokenCharacters[] = $text[$i];
+                        $currentTokenType = self::TOKEN_WS;
+                        $state = 1;
+                        break;
+                    }
+                    if (preg_match($puntRegExp, $text[$i])) {
+                        $currentTokenCharacters[] = $text[$i];
+                        $currentTokenType = self::TOKEN_PUNCT;
+                        $state = 2;
+                        break;
+                    }
+                    $currentTokenCharacters[] = $text[$i];
+                    $currentTokenType = self::TOKEN_WORD;
+                    $state = 3;
+                    break;
+                
+                case 1:
+                    if (preg_match($wsRegExp, $text[$i])) {
+                        $currentTokenCharacters[] = $text[$i];
+                        break;
+                    }
+                    if (preg_match($puntRegExp, $text[$i])) {
+                        $tokens[] = [ 'type' => $currentTokenType, 'text' => implode($currentTokenCharacters)];
+                        $currentTokenCharacters  = [];
+                        $currentTokenCharacters[] = $text[$i];
+                        $currentTokenType = self::TOKEN_PUNCT;
+                        $state = 2;
+                        break;
+                    }
+                    $tokens[] = [ 'type' => $currentTokenType, 'text' => implode($currentTokenCharacters)];
+                    $currentTokenCharacters  = [];
+                    $currentTokenCharacters[] = $text[$i];
+                    $currentTokenType = self::TOKEN_WORD;
+                    $state = 3;
+                    break;
+                    
+                case 2:
+                    if (preg_match($wsRegExp, $text[$i])) {
+                        $tokens[] = [ 'type' => $currentTokenType, 'text' => implode($currentTokenCharacters)];
+                        $currentTokenCharacters  = [];
+                        $currentTokenCharacters[] = $text[$i];
+                        $currentTokenType = self::TOKEN_WS;
+                        $state = 1;
+                        break;
+                    }
+                    if (preg_match($puntRegExp, $text[$i])) {
+                        // punctuation characters generate one token per character
+                        $tokens[] = [ 'type' => $currentTokenType, 'text' => implode($currentTokenCharacters)];
+                        $currentTokenCharacters  = [];
+                        $currentTokenCharacters[] = $text[$i];
+                        $currentTokenType = self::TOKEN_PUNCT;
+                        break;
+                    }
+                    $tokens[] = [ 'type' => $currentTokenType, 'text' => implode($currentTokenCharacters)];
+                    $currentTokenCharacters  = [];
+                    $currentTokenCharacters[] = $text[$i];
+                    $currentTokenType = self::TOKEN_WORD;
+                    $state = 3;
+                    break;
+                    
+                case 3: 
+                    if (preg_match($wsRegExp, $text[$i])) {
+                        $tokens[] = [ 'type' => $currentTokenType, 'text' => implode($currentTokenCharacters)];
+                        $currentTokenCharacters  = [];
+                        $currentTokenCharacters[] = $text[$i];
+                        $currentTokenType = self::TOKEN_WS;
+                        $state = 1;
+                        break;
+                    }
+                    if (preg_match($puntRegExp, $text[$i])) {
+                        $tokens[] = [ 'type' => $currentTokenType, 'text' => implode($currentTokenCharacters)];
+                        $currentTokenCharacters  = [];
+                        $currentTokenCharacters[] = $text[$i];
+                        $currentTokenType = self::TOKEN_PUNCT;
+                        $state = 2;
+                        break;
+                    }
+                    $currentTokenCharacters[] = $text[$i];
+                    break;
+            }
+        }
+        $tokens[] = [ 'type' => $currentTokenType, 'text' => implode($currentTokenCharacters)];
+        return $tokens;
     }
     
     public static function tokenize(array $items) 
@@ -44,9 +155,9 @@ class Tokenizer {
                 continue;
             }
             $n = $item->getAltText();
-            $stringToken = [ 't' => $t, 'n' => ''];
+            $stringToken = [ 't' => trim($t), 'n' => '', 'type' => $item->type];
             if (!self::isWhiteSpace($n)) {
-                $stringToken['n'] = $n;
+                $stringToken['n'] = trim($n);
             }
             $texts[] =  $stringToken;
         }
@@ -54,22 +165,18 @@ class Tokenizer {
         // "explode" the strings
         $tokens = [];
         foreach($texts as $stringToken) {
-            $words = preg_split("/[\s]+/", $stringToken['t']);
-            $wordsN = [];
             if ($stringToken['n'] !== '') {
-                $wordsN = preg_split("/[\s]+/", $stringToken['n']);
-                if (count($words) !== count($wordsN)) {
-                    return false;
-                }
+                // not splitting items with alt text
+                $tokens[] = $stringToken;
+                continue;
             }
+            $words = preg_split("/[\s]+/", $stringToken['t']);
+            
             for ($i = 0; $i < count($words); $i++) {
                 if (self::isWhiteSpace($words[$i])) {
                     continue;
                 }
-                $token = [ 't' => $words[$i]];
-                if ($stringToken['n'] !== '') {
-                    $token['n'] = $wordsN[$i];
-                }
+                $token = [ 't' => $words[$i], 'type' => $stringToken['type']];
                 $tokens[] = $token;
             }
         }
--- src/public/test/TokenizerTest.php
+++ src/public/test/TokenizerTest.php
@@ -34,21 +34,84 @@ use AverroesProject\TxText\Text;
 class TokenizerTest extends TestCase {
      
        
+    public function testSplit()
+    {
+        $testCases = [ 
+            [ 
+                'text' => 'Two words', 
+                'expected' => [
+                    ['type' => Tokenizer::TOKEN_WORD, 'text' => 'Two'], 
+                    ['type' => Tokenizer::TOKEN_WS, 'text' => ' '], 
+                    ['type' => Tokenizer::TOKEN_WORD, 'text' => 'words']
+                ]
+            ],
+            [ 
+                'text' => 'Two. Sentences.', 
+                'expected' => [
+                    ['type' => Tokenizer::TOKEN_WORD, 'text' => 'Two'], 
+                    ['type' => Tokenizer::TOKEN_PUNCT, 'text' => '.'], 
+                    ['type' => Tokenizer::TOKEN_WS, 'text' => ' '], 
+                    ['type' => Tokenizer::TOKEN_WORD, 'text' => 'Sentences'], 
+                    ['type' => Tokenizer::TOKEN_PUNCT, 'text' => '.']
+                ]
+            ],
+            [ 
+                'text' => "\n", 
+                'expected' => [ 
+                    ['type' => Tokenizer::TOKEN_WS, 'text' => "\n"]
+                ],
+            ],
+            [ 
+                'text' => "...to be continued!\n", 
+                'expected' => [ 
+                    ['type' => Tokenizer::TOKEN_PUNCT, 'text' => '.'],
+                    ['type' => Tokenizer::TOKEN_PUNCT, 'text' => '.'],
+                    ['type' => Tokenizer::TOKEN_PUNCT, 'text' => '.'],
+                    ['type' => Tokenizer::TOKEN_WORD, 'text' => 'to'],
+                    ['type' => Tokenizer::TOKEN_WS, 'text' => ' '],
+                    ['type' => Tokenizer::TOKEN_WORD, 'text' => 'be'],
+                    ['type' => Tokenizer::TOKEN_WS, 'text' => ' '],
+                    ['type' => Tokenizer::TOKEN_WORD, 'text' => 'continued'],
+                    ['type' => Tokenizer::TOKEN_PUNCT, 'text' => '!'],
+                    ['type' => Tokenizer::TOKEN_WS, 'text' => "\n"]
+                ],
+            ],
+            [
+                'text' => 'בב"ח ולזה',
+                'expected' => [
+                    ['type' => Tokenizer::TOKEN_WORD, 'text' => 'בב"ח'],
+                    ['type' => Tokenizer::TOKEN_WS, 'text' => ' '],
+                    ['type' => Tokenizer::TOKEN_WORD, 'text' => 'ולזה'],
+                ]
+            ]
+        ];
+        
+        foreach($testCases as $testCase) {
+            $tokens = Tokenizer::splitText($testCase['text']);
+            $this->assertCount(count($testCase['expected']), $tokens);
+            for ($i = 0; $i < count($tokens); $i++) {
+                $this->assertEquals($testCase['expected'][$i]['text'], $tokens[$i]['text']);
+                $this->assertEquals($testCase['expected'][$i]['type'], $tokens[$i]['type']);
+            }
+        }
+        
+    }
+    
     public function testSimpleTokens(){
         
         $items1 = [];
         $items1[] = new Text(1, 0, "This is   a   simple ");
         $items1[] = new TxText\Rubric(2, 1, "rubric   ");
-        $items1[] = new TxText\Sic(3, 2, 'wit a', 'with a');
+        $items1[] = new TxText\Sic(3, 2, 'wit a ', 'with a ');
         $items1[] = new Text(4, 3, "correction");
         
         $tokens1 = Tokenizer::tokenize($items1);
-        //var_dump($tokens1);
-        $expectedTokens1 = ['This', 'is', 'a', 'simple', 'rubric', 'wit', 'a', 'correction'];
+        $expectedTokens1 = ['This', 'is', 'a', 'simple', 'rubric', 'wit a', 'correction'];
         $this->assertCount(count($expectedTokens1), $tokens1);
         for($i = 0; $i < count($tokens1); $i++){
             $this->assertEquals($expectedTokens1[$i], $tokens1[$i]['t']);
         }
+        
     }
 
 }

c0acda38126b03453173c4f0418cef130b1ed475
Rafael Nájera <rafael.najera@uni-koeln.de>
2018-03-22 11:11:18 +0100

    Bumped version to 0.12.0

--- src/public/index.php
+++ src/public/index.php
@@ -45,7 +45,7 @@ require 'config.tables.php';
 
 // Application parameters
 $config['app_name'] = 'Averroes Project Manager';
-$config['version'] = '0.11.2 (2018-Feb-13)';
+$config['version'] = '0.12.0 (2018-Mar-22)';
 $config['copyright_notice'] = <<<EOD
         2016-18, 
         <a href="http://www.thomasinstitut.uni-koeln.de/">

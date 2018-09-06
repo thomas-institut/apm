779aada4a71ec0d563aa5cf589eeacdfcbf8b36e
Rafael Nájera <rafael.najera@uni-koeln.de>
2018-09-06 14:44:11 +0200

    Small template change

--- src/public/classes/ItemStream/ItemStream.php
+++ src/public/classes/ItemStream/ItemStream.php
@@ -1,19 +1,32 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Copyright (C) 2018 Universität zu Köln
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *   
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  
  */
 
 namespace AverroesProject\ItemStream;
 
 use AverroesProject\ColumnElement\Element;
 use AverroesProject\Data\DataManager;
+
 /**
  * Utility class with algorithms for getting text out of item streams
  * 
- * TODO: move to a plugin
  *
  * @author Rafael Nájera <rafael.najera@uni-koeln.de>
  */

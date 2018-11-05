69fd82f52615f4203ca055afb5571fbf7bd9e5e7
Rafael Nájera <rafael@najera.ca>
2018-11-05 12:14:08 +0100

    Commit before going back to the office

---
+++ src/public/classes/apm/Site/SiteCollationTable.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * Copyright (C) 2016-18 Universität zu Köln
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/**
+ * @brief Site Controller class
+ * @author Rafael Nájera <rafael.najera@uni-koeln.de>
+ */
+
+
+namespace APM\Site;
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+use AverroesProject\Profiler\ApmProfiler;
+
+/**
+ * Site Controller class
+ *
+ */
+class SiteCollationTable extends SiteController
+{
+   
+    
+    public function quickCollationPage(Request $request, Response $response, $next)
+    {
+        return $this->ci->view->render($response, 'collation.quick.twig', [
+            'contactName' => $this->ci->settings['support-contact-name'],
+            'contactEmail' => $this->ci->settings['support-contact-email'],
+            'copyright' => $this->ci->copyrightNotice,
+            'baseurl' => $this->ci->settings['baseurl'],
+        ]);
+    }
+}
---
+++ src/public/classes/apm/Site/SiteController.php
@@ -0,0 +1,105 @@
+<?php
+/*
+ * Copyright (C) 2016-18 Universität zu Köln
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/**
+ * @brief Site Controller class
+ * @author Rafael Nájera <rafael.najera@uni-koeln.de>
+ */
+
+
+namespace APM\Site;
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+/**
+ * Site Controller class
+ *
+ */
+class SiteController
+{
+    protected $ci;
+    
+    //Constructor
+    public function __construct( $ci)
+    {
+       $this->ci = $ci;
+       $this->db = $ci->db;
+       $this->hm = $ci->hm;
+       $config = $this->ci->settings;
+       $this->ci->copyrightNotice  = $config['app_name'] . " v" . 
+               $config['version'] . " &bull; &copy; " . 
+               $config['copyright_notice'] . " &bull; " .  
+               strftime("%d %b %Y, %H:%M:%S %Z");
+    }
+
+    protected function genDocPagesListForUser($userId, $docId)
+    {
+        $docInfo = $this->db->getDocById($docId);
+        $url = $this->ci->router->pathFor('doc.showdoc', ['id' => $docId]);
+        $title = $docInfo['title'];
+        $docListHtml = '<li>';
+        $docListHtml .= "<a href=\"$url\" title=\"View Document\">$title</a>";
+        $docListHtml .= '<br/><span style="font-size: 0.9em">';
+        $pageIds = $this->db->getPageIdsTranscribedByUser($userId, $docId);
+
+        $nPagesInLine = 0;
+        $maxPagesInLine = 25;
+        foreach($pageIds as $pageId) {
+            $nPagesInLine++;
+            if ($nPagesInLine > $maxPagesInLine) {
+                $docListHtml .= "<br/>";
+                $nPagesInLine = 1;
+            }
+            $pageInfo = $this->db->getPageInfo($pageId);
+            $pageNum = is_null($pageInfo['foliation']) ? $pageInfo['seq'] : $pageInfo['foliation'];
+            $pageUrl = $this->ci->router->pathFor('pageviewer.docseq', ['doc' => $docId, 'seq'=>$pageInfo['seq']]);
+            $docListHtml .= "<a href=\"$pageUrl\" title=\"View Page\">$pageNum</a>&nbsp;&nbsp;";
+        }
+        $docListHtml .= '</span></li>';
+        
+        return $docListHtml;
+    }
+    
+    // Utility function
+    protected function buildPageArray($pagesInfo, $transcribedPages, $navByPage = true){
+        $thePages = array();
+        foreach ($pagesInfo as $page) {
+            $thePage = array();
+            $thePage['number'] = $page['page_number'];
+            $thePage['seq'] = $page['seq'];
+            $thePage['type'] = $page['type'];
+            if ($page['foliation'] === NULL) {
+                $thePage['foliation'] = '-';
+            } else {
+                $thePage['foliation'] = $page['foliation'];
+            }
+            
+            $thePage['classes'] = '';
+            if (array_search($page['page_number'], $transcribedPages) === FALSE){
+                $thePage['classes'] = 
+                        $thePage['classes'] . ' withouttranscription';
+            }
+            $thePage['classes'] .= ' type' . $page['type'];
+            array_push($thePages, $thePage);
+        }
+        return $thePages;
+    }
+    
+}

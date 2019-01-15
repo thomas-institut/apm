37eb7b39b7421f743e60643de721472db6c622f9
Rafael Nájera <rafael.najera@uni-koeln.de>
2019-01-15 09:57:33 +0100

    Added Note objects to APM Item's

---
+++ src/public/classes/apm/Core/Item/Note.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * Copyright (C) 2016-18 Universität zu Köln
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+namespace APM\Core\Item;
+
+/**
+ * Simple representation of an editorial note: a text by an author with a given 
+ * time.
+ * 
+ * @author Rafael Nájera <rafael.najera@uni-koeln.de>
+ */
+class Note {
+    
+    protected $author;
+    const AUTHOR_UNDEFINED = false;
+    
+    protected $text;
+    const TEXT_NOTEXT = '[empty]';
+    
+    /** @var int */
+    protected $time;
+    const TIME_NOW = -1;
+    
+    public function __construct($text = self::TEXT_NOTEXT, $author = self::AUTHOR_UNDEFINED, int $time = self::TIME_NOW) {
+        $this->setAuthor($author);
+        $this->setTime($time);
+        $this->setText($text);
+    }
+    
+    public function getText() {
+        return $this->text;
+    }
+    
+    public function setText($text) {
+        if ($text === '') {
+            throw new \InvalidArgumentException('Cannot set a Note\'s text to an empty string');
+        }
+        $this->text = $text;
+    }
+    
+    public function getAuthor() {
+        return $this->author;
+    }
+    
+    public function setAuthor($author) {
+        $this->author = $author;
+    }
+    
+    public function getTime() : int {
+        return $this->time;
+    }
+    
+    public function setTime(int $time = self::TIME_NOW) {
+        if ($time === self::TIME_NOW) {
+            $time = time();
+        }
+        $this->time = $time;
+    }
+}

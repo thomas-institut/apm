42d2c56307c85ac55c2c8bb9c883cf01a276dbed
Rafael Nájera <rafael.najera@uni-koeln.de>
2019-01-24 12:25:21 +0100

    Bumped version to v0.19.2

--- src/public/index.php
+++ src/public/index.php
@@ -43,7 +43,7 @@ use AverroesProject\Collatex\CollatexRunner;
 
 // Application parameters
 $config['app_name'] = 'Averroes Project Manager';
-$config['version'] = '0.19.1 (2019-Jan-18)';
+$config['version'] = '0.19.2 (2019-Jan-24)';
 $config['copyright_notice'] = <<<EOD
         2016-19, 
         <a href="http://www.thomasinstitut.uni-koeln.de/">

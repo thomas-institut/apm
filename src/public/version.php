72310b7643d391b8860c29b91927425f9b7b2f2a
Rafael Nájera <rafael.najera@uni-koeln.de>
2019-05-21 10:07:02 +0200

    Bumped version to 0.20.2

--- src/public/version.php
+++ src/public/version.php
@@ -21,7 +21,7 @@
 
 
 $config['app_name'] = 'Averroes Project Manager';
-$config['version'] = '0.20.1 (2019-May-20)';
+$config['version'] = '0.20.2 (2019-May-21)';
 
 
 $config['copyright_notice'] = <<<EOD

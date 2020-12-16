a277b9784cc3b29fefdfd084757d8830429b193f
Rafael Nájera <rafael@najera.ca>
2020-12-16 13:06:34 +0100

    Bumped version to 0.37.1

--- src/public/version.php
+++ src/public/version.php
@@ -26,7 +26,7 @@ global $config;
 
 
 $config[ApmConfigParameter::APP_NAME] = 'APM';
-$config[ApmConfigParameter::VERSION] = '0.37.0 (2020-Dec-14)';
+$config[ApmConfigParameter::VERSION] = '0.37.1 (2020-Dec-16)';
 
 
 $config[ApmConfigParameter::COPYRIGHT_NOTICE] = <<<EOD

2598d74e7821876cfa4119eb66d6905c2f2b30d4
Rafael Nájera <rafael.najera@uni-koeln.de>
2021-10-04 09:42:59 +0000

    Bumped version to 0.42.7

--- src/public/version.php
+++ src/public/version.php
@@ -26,7 +26,7 @@ global $config;
 
 
 $config[ApmConfigParameter::APP_NAME] = 'APM';
-$config[ApmConfigParameter::VERSION] = '0.42.6 (2021-Sep-20)';
+$config[ApmConfigParameter::VERSION] = '0.42.7 (2021-Oct-04)';
 
 
 $config[ApmConfigParameter::COPYRIGHT_NOTICE] = <<<EOD

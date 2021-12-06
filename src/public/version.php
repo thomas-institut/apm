0e9d33bf8a576c0468fecb8dd21887bfc68a334f
Rafael Nájera <rafael.najera@uni-koeln.de>
2021-12-06 09:13:03 +0100

    Bumped version to 0.44.0

--- src/public/version.php
+++ src/public/version.php
@@ -29,7 +29,7 @@ $config[ApmConfigParameter::APP_NAME] = 'APM';
 // Version 0.42.10, named after Oded, who discovered the first reproducible bug after EditionComposer's bug detector
 // was implemented
 // Version 0.42.11 still named after Oded, since he also found the bug that caused the update!
-$config[ApmConfigParameter::VERSION] = '0.43.2 (2021-Nov-24)';
+$config[ApmConfigParameter::VERSION] = '0.44.0 (2021-Nov-24)';
 
 
 $config[ApmConfigParameter::COPYRIGHT_NOTICE] = <<<EOD

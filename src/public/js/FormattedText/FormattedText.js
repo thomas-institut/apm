7a14082e8595a4b0f5c0107191ef3f513c62edac
Rafael Nájera <rafael.najera@uni-koeln.de>
2022-02-15 09:52:57 +0100

    Started implementing new FormattedText

---
+++ src/public/js/FormattedText/FormattedText.js
@@ -0,0 +1,30 @@
+
+
+
+
+class FormattedText {
+
+  /**
+   *
+   * @param {string | array }text
+   */
+  constructor (text = '') {
+
+  }
+
+
+
+}
+
+
+function getTokenArrayFromAnything(anything) {
+
+  if (typeof anything === 'string') {
+    return
+  }
+
+}
+
+function createTokenFromString(someString) {
+
+}

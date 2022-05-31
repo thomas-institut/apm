d816e75ae4eb9b1846bf7f73f1607ead72ba4887
Lukas (Office) <luk.reichert@posteo.de>
2022-05-31 13:04:46 +0200

    Search function optimated for displaying search result with more than one match

--- src/public/classes/apm/Api/TEST_OpenSearch.php
+++ src/public/classes/apm/Api/TEST_OpenSearch.php
@@ -17,66 +17,61 @@ $client = (new \OpenSearch\ClientBuilder())
 
 $indexName = 'philosophers';
 
-/*
-// Clear/Delete index
-$client->indices()->delete([
-    'index' => $indexName
-]);
-
-// Create an index with non-default settings.
-$client->indices()->create([
-    'index' => $indexName,
-]);
-
-$client->create([
-    'index' => $indexName,
-    'id' => 1,
-    'body' => [
-        'author' => 'Kant',
-        'opus' => 'Kritik der reinen Vernunft',
-        'year' => 1787
-    ],
-]);
-
-$client->create([
-    'index' => $indexName,
-    'id' => 2,
-    'body' => [
-        'author' => 'Cassirer',
-        'opus' => 'Philosophie der symbolischen Formen',
-        'year' => 1923
-    ],
-]);
-*/
-
 // Function to search the index
-$indexName = 'philosophers';
+
 function search_for_keyword ($keyword)
     {
         global $client, $indexName;
 
+        // Query the index
         $result = $client->search([
                 'index' => $indexName,
                 'body' => [
                     'query' => [
                         'multi_match' => [
                             'query' => $keyword,
-                            'fields' => ['author', 'opus']
+                            'fields' => ['author', 'book']
                         ]
                     ]
                 ]
             ]);
 
-        if ($result['hits']['total']['value'] == 0) {
+
+        if ($result['hits']['total']['value'] == 0) { // no matches for searched keyword
             return "There is no match in the database for the keyword '$keyword'.";
         }
-        else {
+        elseif ($result['hits']['total']['value'] == 1) { // exactly one match for the searched keyword
             $docID = $result['hits']['hits']['0']['_id'];
             $author = $result['hits']['hits']['0']['_source']['author'];
             $book = $result['hits']['hits']['0']['_source']['book'];
-            return "The book '$book' of '$author' matches your search. The corresponding document has the ID $docID.";
+            return "There is one match for your search, namely the book '$book' of '$author' (Document ID $docID).";
+        }
+        else{ // more than one match for the searched keyword
+            $numMatches = $result['hits']['total']['value'];
+            $docIDs = [];
+            $authors = [];
+            $books = [];
+            $enumMatches = "";
+
+            // Collect all matches and enumerate them in a string
+            for ($i = 0; $i <= $numMatches-1; $i++) {
+                $docIDs[$i] = $result['hits']['hits'][$i]['_id'];
+                $authors[$i] = $result['hits']['hits'][$i]['_source']['author'];
+                $books[$i] = $result['hits']['hits'][$i]['_source']['book'];
+                if ($i != $numMatches-1 and $i != $numMatches-2) { // affects all matches besides penultimate and ultimate match
+                    $enumMatches = $enumMatches . "the book '$books[$i]' of '$authors[$i]' (Document ID $docIDs[$i]), ";
+                }
+                elseif ($i != $numMatches-1) { // affects penultimate match
+                    $enumMatches = $enumMatches . "the book '$books[$i]' of '$authors[$i]' (Document ID $docIDs[$i]) ";
+                }
+                else { // affects ultimate match
+                    $enumMatches = $enumMatches . "and the book '$books[$i]' of '$authors[$i]' (Document ID $docIDs[$i]).";
+                }
+            }
+
+            return "There are $numMatches matches for your search, namely " . $enumMatches;
         }
     }
 
 // Search for keyword and print the result
-print_r(search_for_keyword("Kant"));
+print_r(search_for_keyword("Vernunft"));

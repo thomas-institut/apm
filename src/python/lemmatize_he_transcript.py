03e768d7005a06c5f659047d1110086de9f90615
rauchert <luk.reichert@posteo.de>
2022-10-10 12:08:47 +0200

    Hebrew lemmatization outside  of the APM possible

--- src/python/lemmatize_he_transcript.py
+++ src/python/lemmatize_he_transcript.py
@@ -1,5 +1,58 @@
-from cube.api import Cube       # import the Cube object
-cube=Cube(verbose=True)         # initialize it
-cube.load("he", device='cpu')   # select the desired language (it will auto-download the model on first run)
-text="This is the text I want segmented, tokenized, lemmatized and annotated with POS and dependencies."
-document=cube(text)            # call with your own text (string) to obtain the annotations
+"""
+
+LEMMATIZATION WITH SPARK NLP (Seems to work,but needs to much time)
+
+from pyspark.ml import Pipeline
+import sparknlp
+from sparknlp.annotator import *
+from sparknlp.base import DocumentAssembler, LightPipeline
+
+spark = sparknlp.start()
+
+document_assembler = DocumentAssembler()\
+    .setInputCol("text")\
+    .setOutputCol("document")
+
+tokenizer = Tokenizer() \
+    .setInputCols(["document"]) \
+    .setOutputCol("token")
+
+lemmatizer_he = LemmatizerModel.pretrained("lemma", "he") \
+        .setInputCols(["token"]) \
+        .setOutputCol("lemma")
+
+pipeline_he = Pipeline(stages=[document_assembler, tokenizer, lemmatizer_he])
+
+transcript = ["האריגה לעכביש. ובתים המשושי׳ לדבורים אלא שההבדל ביניהם הוא שהם מגיעות באדם במחשבה וההוצאה מהלב וזהו שנשלם בו בצלאל באמרו ולחשוב מחשבות ויוני מגיעות מהטבע ולכן לא ימצאו פעולתם במיני׳ שונים אבל ישיג "]
+
+data_he = spark.createDataFrame([transcript]).toDF("text")
+light_pipeline_he = LightPipeline(pipeline_he.fit(data_he))
+
+result_he = light_pipeline_he.annotate(transcript)
+
+tokens = [content['token'] for content in result_he]
+lemmata = [content['lemma'] for content in result_he]
+
+
+print(tokens)
+print(lemmata)  
+"""
+
+import spacy_udpipe
+
+# spacy_udpipe.download("he")
+
+transcript="ולחשוב כי בדעת הוא מדבר. ולב שמעו יפותה מאמרו: ואם נמצא בנצח לשונו. במאמרים מפורסמים חברו. אשר יודו בני איש בם ויכיר. לחסרונם חכם לבב מהירו. ואם אין זה הֲדעת חק ומשפט שלימות זה ומותרו ואסרו. ואם נברא להתחכם ולעסוק. בדין ממון וקלו וחמורו. להבין איך יהיה משפט מריבים. עלי דבר ונזק שור ובורו. וקרבן שגגת איש או זדונו"
+nlp = spacy_udpipe.load("he")
+
+annotations = nlp(transcript)
+tokens = [token.text for token in annotations]
+lemmata = [token.lemma_ for token in annotations]
+
+print(tokens)
+print(lemmata)
+
+if (len(tokens) == len(lemmata)):
+    print ("Success!")
+else:
+    print ("Error!")

2d6b6000ef081070f50cd368bc8763e72734ac58
Rafael Nájera <rafael@najera.ca>
2023-03-23 15:03:57 +0100

    More on APMng

--- src/apm_ng/src/views/Users.vue
+++ src/apm_ng/src/views/Users.vue
@@ -1,12 +1,34 @@
 <template>
   <h1>Users</h1>
-  <p>Users go here!</p>
+  <ul>
+    <li v-for="user in userList">
+      {{user}}
+    </li>
+  </ul>
 </template>
 
-<script>
-export default {
-  name: 'Users'
-}
+<script setup>
+import { onMounted, ref } from 'vue'
+
+ let userList = ref([])
+ let loaded = ref(false)
+
+ onMounted(() =>{
+   getUsers()
+ })
+
+ async function getUsers() {
+  console.log(`Executing getUsers, loaded= ${loaded.value}`)
+   if (!loaded.value) {
+     console.log('Getting users')
+     setTimeout( () => {
+       console.log("Got users")
+       userList.value = [ 'Peter', 'Paul', 'Mary']
+       loaded.value = true
+     }, 2000)
+   }
+ }
+
 </script>
 
 <style scoped>

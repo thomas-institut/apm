d09e3765a88d29ba8c36ea40b3023a1069d00366
Rafael Nájera <rafael@najera.ca>
2023-12-18 12:42:18 +0100

    Bumped version to 0.62

--- src/www/version.php
+++ src/www/version.php
@@ -27,7 +27,7 @@ $config[ApmConfigParameter::APP_NAME] = 'APM';
 // Version 0.42.10, named after Oded, who discovered the first reproducible bug after EditionComposer's bug detector
 // was implemented
 // Version 0.42.11 still named after Oded, since he also found the bug that caused the update!
-$config[ApmConfigParameter::VERSION] = '0.61.5 (2023-Nov-20)';
+$config[ApmConfigParameter::VERSION] = '0.62.0 (2023-Dec-18)';
 
 
 $config[ApmConfigParameter::COPYRIGHT_NOTICE] = <<<EOD

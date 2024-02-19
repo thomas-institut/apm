0b710d2325389109ffd6adf1440a76ecc9d06c22
Rafael Nájera <rafael@najera.ca>
2024-02-19 11:07:10 +0100

    Bumped version to 0.63.1

--- src/www/version.php
+++ src/www/version.php
@@ -32,7 +32,7 @@ $config[ApmConfigParameter::APP_NAME] = 'APM';
 //   was implemented
 // + Version 0.42.11 still named after Oded, since he also found the bug that caused the update!
 //
-$config[ApmConfigParameter::VERSION] = '0.63.0 (2024-Feb-19)';
+$config[ApmConfigParameter::VERSION] = '0.63.1 (2024-Feb-19)';
 
 //
 // Copyright Notice
